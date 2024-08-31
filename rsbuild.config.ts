715ea36d4bbaa8c4582d1c9d32a1c4a9c3bed5d7
Jhorman Tito <jhormanrus@gmail.com>
2024-08-31 15:06:22 -0500

    use builtin postcss and autoprefixer

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -8,6 +8,24 @@ import { name, dependencies } from './package.json'
 const publicPath = process.env.VUE_APP_DOMAIN
 const { publicVars } = loadEnv({ prefixes: ['VUE_APP_'] })
 
+const moduleFederationOptions = {
+  name: proyect_name,
+  filename: 'remoteEntry.js',
+  remotes: {
+    app_alegra_commons: `app_alegra_commons@https://${prefixEnvironment}alegra-commons.alegra.com/remoteEntry.js?v=[('0' + (new Date().getMonth()+1)).slice(-2)+('0'+new Date().getDate()).slice(-2)+new Date().getHours()]`,
+  },
+  exposes: {
+    './microfront': './src/micro/mount',
+  },
+  shared: {
+    'lodash-es': { singleton: true, requiredVersion: '4.17.21' },
+    pinia: { singleton: true, requiredVersion: '2.0.14' },
+    vue: { singleton: true, requiredVersion: '3.4.21' },
+    'vue-router': { singleton: true, requiredVersion: '4.3.0' },
+    '@alegradev/smile-ui-next': dependencies['@alegradev/smile-ui-next'],
+  },
+}
+
 export default defineConfig({
   plugins: [pluginNodePolyfill(), pluginSass(), pluginVue()],
   html: {
@@ -39,22 +57,6 @@ export default defineConfig({
     },
   },
   moduleFederation: {
-    options: {
-      name: proyect_name,
-      filename: 'remoteEntry.js',
-      remotes: {
-        app_alegra_commons: `app_alegra_commons@https://${prefixEnvironment}alegra-commons.alegra.com/remoteEntry.js?v=[('0' + (new Date().getMonth()+1)).slice(-2)+('0'+new Date().getDate()).slice(-2)+new Date().getHours()]`,
-      },
-      exposes: {
-        './microfront': './src/micro/mount',
-      },
-      shared: {
-        'lodash-es': { singleton: true, requiredVersion: '4.17.21' },
-        pinia: { singleton: true, requiredVersion: '2.0.14' },
-        vue: { singleton: true, requiredVersion: '3.4.21' },
-        'vue-router': { singleton: true, requiredVersion: '4.3.0' },
-        '@alegradev/smile-ui-next': dependencies['@alegradev/smile-ui-next'],
-      },
-    },
+    options: moduleFederationOptions,
   },
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { theme } = require('@alegradev/smile-ui-next')
 const { proyect_name } = require('./config/index')
 
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts}'],
   theme: theme(),
   plugins: [],
   important: `.${proyect_name}`,
