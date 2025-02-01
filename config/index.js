27e61add5d7e586fcebc354062f752bda68884e2
Nicolas Montoya <nicolas.montoya@alegra.com>
2025-02-01 17:08:52 -0300

    chore: setup project

--- config/index.js
+++ config/index.js
@@ -13,6 +13,6 @@ function getEnvironmentPrefix() {
 
 module.exports = {
   proyect_port: 1026,
-  proyect_name: 'app_alegra_template',
+  proyect_name: 'reports_ai_chat',
   prefixEnvironment: getEnvironmentPrefix(),
 }
